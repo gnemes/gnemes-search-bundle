0f3b64386deffb7feae9e261ebdff88ca506e7c0
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-02-29 17:27:56 -0300

    Search form

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,13 +1,77 @@
 <?php
+/**
+ * Gnemes Search Bundle
+ * Copyright (C) 2016  German Nemes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP VERSION 5.6
+ *
+ * @category  Bundle
+ * @package   GnemesSearchBundle
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/aprildesign
+ */
 
 namespace Gnemes\SearchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Gnemes\SearchBundle\Entity\Needle;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
+/**
+ * Gnemes Search Bundle controller Class
+ *
+ * @category  Bundle
+ * @package   GnemesSearchBundle
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/aprildesign
+ */
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        // Init needle entity
+        $needle = new Needle();
+        
+        // Create form
+        $form = $this->createFormBuilder($needle)
+            ->setAction($this->generateUrl('gnemes_search_homepage'))
+            ->setMethod('POST')    
+            ->add(
+                'name', 
+                TextType::class, 
+                array(
+                    'label' => 'Text to search'
+                )
+            )
+            ->add('save', SubmitType::class, array('label' => 'Search'))
+            ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            echo "ou yeah!";
+        }
+        
+        // Template vars
         $params = array();
         $params["gnemes"] = array(
             "title" => "Gnemes text in file search engine",
@@ -17,5 +81,10 @@ class DefaultController extends Controller
             'GnemesSearchBundle:Default:index.html.twig', 
             $params
         );
+    }
+    
+    public function searchAction(Request $request)
+    {
+        
     }
 }
