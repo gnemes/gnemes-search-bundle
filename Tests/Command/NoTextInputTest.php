8ea26fc7c0a27066f361db5ee5e50562d94a7fdd
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-01 11:03:14 -0300

    Command test added

---
+++ Tests/Command/NoTextInputTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Gnemes Search Bundle
+ * Copyright (C) 2016  German Nemes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP VERSION 5.6
+ *
+ * @category  Bundle/Test/Command
+ * @package   GnemesSearchBundle
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/aprildesign
+ */
+
+namespace Tests\Gnemes\SearchBundle\Command;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Gnemes\SearchBundle\Command\SearchCommand;
+
+class NoTextInputTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        // mock the Kernel or create one depending on your needs
+        $application = new Application($kernel);
+        $application->add(new SearchCommand());
+
+        $command = $application->find('gnemes:search');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            array(
+                'name'    => '',
+             )
+        );
+
+        $this->assertRegExp('/.../', $commandTester->getDisplay());
+
+        // ...
+    }
+}
