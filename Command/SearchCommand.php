2e17f6ff7e04e04d1a2083ba5e8ea5499401010a
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-01 08:48:29 -0300

    Command skeleton

---
+++ Command/SearchCommand.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Gnemes Search Bundle
+ * Copyright (C) 2016  German Nemes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP VERSION 5.6
+ *
+ * @category  Bundle/Command
+ * @package   GnemesSearchBundle
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/aprildesign
+ */
+
+namespace Gnemes\SearchBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Gnemes Search Bundle command Class
+ *
+ * @category  Bundle/Command
+ * @package   GnemesSearchBundle
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/aprildesign
+ */
+class GreetCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('gnemes:search')
+            ->setDescription('Greet someone')
+            ->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                'Who do you want to greet?'
+            )
+            ->addOption(
+                'yell',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the task will yell in uppercase letters'
+            )
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+}    
