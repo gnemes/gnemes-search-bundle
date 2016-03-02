d7550e18610be3f15c697abf51c30cbac8eeb527
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-01 22:29:26 -0300

    Moving search controller to a service class

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,6 +23,12 @@ class Configuration implements ConfigurationInterface
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
+        $rootNode
+            ->children()
+                ->integerNode('source')->end()
+            ->end()
+        ;
+        
 
         return $treeBuilder;
     }
