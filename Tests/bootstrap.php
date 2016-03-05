d481af94dedd185b8775736d4a5bf2350ae684ad
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-04 21:45:07 -0300

    testing basic files

---
+++ Tests/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Gnemes Search Bundle
+ * Copyright (C) 2016  German Nemes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP VERSION 5.6
+ *
+ * @category  Bundle/Tests
+ * @package   GnemesSearchBundle
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/aprildesign
+ */
+
+if (!($loader = @include __DIR__ . '/../vendor/autoload.php')) {
+    echo <<<EOT
+You need to install the project dependencies using Composer:
+$ wget http://getcomposer.org/composer.phar
+OR
+$ curl -s https://getcomposer.org/installer | php
+$ php composer.phar install --dev
+$ phpunit
+EOT;
+    exit(1);
+}
